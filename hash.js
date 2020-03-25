ee1c44a0e6e71090dbc49889280bb2b9c8c4b4a7
Arnaud.Royo <arnaud.royo@epsi.fr>
2020-03-25 12:11:30 +0100

    add project

---
+++ hash.js
@@ -0,0 +1,26 @@
+const crypto = require('crypto');
+var fs = require('fs');
+
+
+
+
+// Get process.stdin as the standard input object.
+var standard_input = process.stdin;
+
+standard_input.setEncoding('utf-8');
+console.log("Please input ur password.");
+
+// When user input data and click enter key.
+standard_input.on('data', function (data) {
+        const hash = crypto.createHmac('sha256', data)
+                           .update('MySalt')
+                           .digest('hex');
+        console.log('hash(256): ' +hash);
+
+    //update file
+    fs.appendFile('myHashingFile.txt',hash + '\n', function (err) {
+        if (err) throw err;
+        console.log('Saved in file! Type another passoword:');
+    });
+});
+
